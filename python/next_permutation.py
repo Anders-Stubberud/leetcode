d3e09a241e4a26732331c1b78ee3113bbedc1c4c
Anders <andersvs02@gmail.com>
2023-09-10 10:36:58 +0200

    next_permutation.py

--- python/next_permutation.py
+++ python/next_permutation.py
@@ -1,24 +1,26 @@
+import math
 def nextPermutation(nums):
     """
     Do not return anything, modify nums in-place instead.
     """
-
-    index_candidate = len(nums) - 1
-    index_swapper = len(nums) - 2
-    while nums[index_candidate] < nums[index_swapper] and index_swapper >= 0:
-        index_swapper -= 1
-        #finne minste som er større enn swapper
-    if index_swapper >= 0:
-        nums[index_swapper], nums[index_candidate] = nums[index_candidate], nums[index_swapper]
-
-    for i in range(index_swapper + 2, len(nums)):
+    index_swap = len(nums) - 2
+    while nums[index_swap + 1] <= nums[index_swap] and index_swap >= 0:
+        index_swap -= 1
+    if index_swap >= 0:
+        smallest_largest_index = index_swap + 1
+        for i in range(index_swap+2, len(nums)):
+            if nums[i] < nums[smallest_largest_index] and nums[i] > nums[index_swap]:
+                smallest_largest_index = i
+        nums[index_swap], nums[smallest_largest_index] = nums[smallest_largest_index], nums[index_swap]
+    for i in range(index_swap + 2, len(nums)):
         key = nums[i]
-        j = i - 1
-        while j >= index_swapper + 1 and key < nums[j]:
-            nums[ j + 1 ] = nums[ j ]
+        j = i-1
+        while j >= index_swap + 1 and key < nums[j]:
+            nums[j + 1] = nums[j]
             j -= 1
-        nums[ j + 1 ] = key
-
+        nums[j+1] = key
+        
+    print(index_swap)
     print(nums)
 
-nextPermutation([2,3,1])
+nextPermutation([5,1,1])
